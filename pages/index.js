30b1163adf3d14aa6d80e6dffbfe2d52cd913d2b
k <k@Usuhuu.local>
2023-07-31 18:31:27 +0800

    tron game done

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,23 @@
 import { useEffect, useState } from "react";
+import * as React from "react";
 import useInterval from "use-interval";
-import Box from "@mui/material/Box";
-import InputLabel from "@mui/material/InputLabel";
-import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
-import Select from "@mui/material/Select";
-
-const zoom = 20;
-
-const areaWidth = 30;
-
-const areaHeight = 30;
+const zoom = 15;
+const areaWidth = 80;
+const areaHeight = 60;
 
 export default function Home() {
   const [body, setbody] = useState([
-    { top: 15, left: 7 },
-    { top: 15, left: 6 },
-    { top: 15, left: 5 },
+    { top: 29, left: 10 },
+    { top: 29, left: 9 },
+    { top: 29, left: 8 },
   ]);
   const [bodyTwo, setbodyTwo] = useState([
-    { top: 15, left: 23 },
-    { top: 15, left: 24 },
-    { top: 15, left: 25 },
+    { top: 29, left: 73 },
+    { top: 29, left: 74 },
+    { top: 29, left: 75 },
   ]);
-
-  const [direction, setDirection] = useState("up");
-
-  const [directionTwo, setDirectionTwo] = useState("down");
+  const [direction, setDirection] = useState("right");
+  const [directionTwo, setDirectionTwo] = useState("left");
   useEffect((e) => {
     window.addEventListener(`keydown`, (e) => {
       setDirection((prevDirection) => {
@@ -54,30 +45,26 @@ export default function Home() {
         }
         return prevDirection;
       });
-    });
-  }, []);
-  useEffect((e) => {
-    window.addEventListener(`keydown`, (e) => {
       setDirectionTwo((prevDirectionTwo) => {
         switch (e.code) {
           case "ArrowLeft":
             if (prevDirectionTwo !== `right`) {
-              return "leftTwo";
+              return "left";
             }
             break;
           case "ArrowRight":
             if (prevDirectionTwo !== `left`) {
-              return "rightTwo";
+              return "right";
             }
             break;
           case "ArrowUp":
             if (prevDirectionTwo !== `down`) {
-              return `upTwo`;
+              return `up`;
             }
             break;
           case "ArrowDown":
             if (prevDirectionTwo !== `up`) {
-              return `downTwo`;
+              return `down`;
             }
             break;
         }
@@ -86,156 +73,158 @@ export default function Home() {
     });
   }, []);
 
-  useInterval(() => {
-    end();
-  }, 100);
-
-  const end = () => {
-    const deleteSnake = [...body];
-    deleteSnake.shift();
-    if (
-      deleteSnake.find((e) => e.top === body[0].top && e.left === body[0].left)
-    ) {
-      alert("Game Over");
-      location.reload();
-    } else if (
-      deleteSnake.find(
-        () => bodyTwo[0].top === body[0].top && bodyTwo[0].left === body[0].left
-      )
-    ) {
-      alert("Game Over");
-      location.reload();
-    } else if (
-      deleteSnake.find(
-        (e) => e.top === bodyTwo[0].top && e.left === bodyTwo[0].left
-      )
-    ) {
-      alert("Game Over");
-      location.reload();
-    }
-  };
-
   function goRight() {
-    const newbody = [...body];
-    let newLeft = newbody[0].left + 1;
+    let newLeft = body[0].left + 1;
     if (newLeft > areaWidth - 1) {
       newLeft = 0;
     }
-    newbody.unshift({ ...newbody[0], left: newLeft });
-    setbody(newbody);
+    return { ...body[0], left: newLeft };
   }
 
   function goDown() {
-    const newbody = [...body];
-    let newTop = newbody[0].top + 1;
+    let newTop = body[0].top + 1;
     if (newTop > areaHeight - 1) {
       newTop = 0;
     }
-    newbody.unshift({ ...newbody[0], top: newTop });
-    setbody(newbody);
+    return { ...body[0], top: newTop };
   }
 
   function goLeft() {
-    const newbody = [...body];
-    let newRight = newbody[0].left - 1;
-
+    let newRight = body[0].left - 1;
     if (newRight < 0) {
       newRight = areaWidth - 1;
     }
-    newbody.unshift({ ...newbody[0], left: newRight });
-    setbody(newbody);
+    return { ...body[0], left: newRight };
   }
   function goUp() {
-    const newbody = [...body];
-    let newBottom = newbody[0].top - 1;
+    let newBottom = body[0].top - 1;
     if (newBottom < 0) {
       newBottom = areaHeight - 1;
     }
-    newbody.unshift({ ...newbody[0], top: newBottom });
-    setbody(newbody);
-  }
 
+    return { ...body[0], top: newBottom };
+  }
   function goRightTwo() {
-    const newbodyTwo = [...bodyTwo];
-    let newLeft = newbodyTwo[0].left + 1;
+    let newLeft = bodyTwo[0].left + 1;
     if (newLeft > areaWidth - 1) {
       newLeft = 0;
     }
-    newbodyTwo.unshift({ ...newbodyTwo[0], left: newLeft });
-    setbodyTwo(newbodyTwo);
+    return { ...bodyTwo[0], left: newLeft };
   }
 
   function goDownTwo() {
-    const newbodyTwo = [...bodyTwo];
-    let newTop = newbodyTwo[0].top + 1;
+    let newTop = bodyTwo[0].top + 1;
     if (newTop > areaHeight - 1) {
       newTop = 0;
     }
-    newbodyTwo.unshift({ ...newbodyTwo[0], top: newTop });
-    setbodyTwo(newbodyTwo);
+    return { ...bodyTwo[0], top: newTop };
   }
 
   function goLeftTwo() {
-    const newbodyTwo = [...bodyTwo];
-    let newRight = newbodyTwo[0].left - 1;
-
+    let newRight = bodyTwo[0].left - 1;
     if (newRight < 0) {
       newRight = areaWidth - 1;
     }
-    newbodyTwo.unshift({ ...newbodyTwo[0], left: newRight });
-    setbodyTwo(newbodyTwo);
+    return { ...bodyTwo[0], left: newRight };
   }
   function goUpTwo() {
-    const newbodyTwo = [...bodyTwo];
-    let newBottom = newbodyTwo[0].top - 1;
+    let newBottom = bodyTwo[0].top - 1;
     if (newBottom < 0) {
       newBottom = areaHeight - 1;
     }
-    newbodyTwo.unshift({ ...newbodyTwo[0], top: newBottom });
-    setbodyTwo(newbodyTwo);
+
+    return { ...bodyTwo[0], top: newBottom };
   }
   useInterval(() => {
+    const newbody = [...body];
+    const newbodyTwo = [...bodyTwo];
+    let newHead = null;
+    let newHeadTwo = null;
     switch (direction) {
       case "right":
-        goRight();
+        newHead = goRight();
         break;
       case "down":
-        goDown();
+        newHead = goDown();
         break;
       case "left":
-        goLeft();
+        newHead = goLeft();
         break;
       case "up":
-        goUp();
+        newHead = goUp();
         break;
+      default:
+        newHead = goRight();
     }
-  }, 300);
-  useInterval(() => {
     switch (directionTwo) {
-      case "rightTwo":
-        goRightTwo();
+      case "right":
+        newHeadTwo = goRightTwo();
         break;
-      case "downTwo":
-        goDownTwo();
+      case "down":
+        newHeadTwo = goDownTwo();
         break;
-      case "leftTwo":
-        goLeftTwo();
+      case "left":
+        newHeadTwo = goLeftTwo();
         break;
-      case "upTwo":
-        goUpTwo();
+      case "up":
+        newHeadTwo = goUpTwo();
         break;
+      default:
+        newHeadTwo = goRightTwo();
     }
-  }, 300);
+
+    const deletePlayerOne = [...body];
+    const deletePlayerTwo = [...bodyTwo];
+    deletePlayerOne.shift();
+    deletePlayerTwo.shift();
+    if (
+      deletePlayerTwo.find(
+        (e) => e.top === body[0].top && e.left === body[0].left
+      )
+    ) {
+      // alert("Player 2 win");
+      location.reload();
+    } else if (
+      deletePlayerOne.find(
+        (e) => e.top === bodyTwo[0].top && e.left === bodyTwo[0].left
+      )
+    ) {
+      alert("Player 1 win");
+      location.reload();
+    } else if (
+      deletePlayerOne.find(
+        (e) => e.top === body[0].top && e.left === body[0].left
+      )
+    ) {
+      alert("Player 2 win");
+      location.reload();
+    } else if (
+      deletePlayerTwo.find(
+        (e) => e.top === bodyTwo[0].top && e.left === bodyTwo[0].left
+      )
+    ) {
+      alert("Player 1 win");
+      location.reload();
+    }
+
+    newbody.unshift(newHead);
+    setbody(newbody);
+    newbodyTwo.unshift(newHeadTwo);
+    setbodyTwo(newbodyTwo);
+  }, 100);
 
   return (
-    <main className={`flex min-h-screen flex-col items-center  p-24`}>
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between p-24`}
+    >
       <div
         className="relative bg-slate-300"
         style={{ width: areaWidth * zoom, height: areaHeight * zoom }}
       >
+        {" "}
         {body.map((segment) => (
           <div
-            className="absolute rounded bg-slate-900"
+            className="absolute rounded bg-blue-900"
             style={{
               top: segment.top * zoom,
               left: segment.left * zoom,
@@ -246,7 +235,7 @@ export default function Home() {
         ))}
         {bodyTwo.map((segment) => (
           <div
-            className="absolute rounded bg-slate-900"
+            className="absolute rounded bg-pink-900"
             style={{
               top: segment.top * zoom,
               left: segment.left * zoom,
